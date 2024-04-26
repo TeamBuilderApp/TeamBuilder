19b029c61f8ae1394c34d85199b788eca79de07c
Chris Peterson <teambuilderapp2025@gmail.com>
2024-04-26 16:31:10 -0400

    Our first check-in frorm the new Repo! Added a Util file

---
+++ TeamBuilder/Util/Util.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamBuilder.Util
+{
+    public static class Util
+    {
+        static Util() 
+        {
+            //If there's ever a need for a Utility file!
+        }
+    }
+}
