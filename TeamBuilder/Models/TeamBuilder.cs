2755fc125d0b652228a9df5dbf3131801a9da012
Chris Peterson <teambuilderapp2025@gmail.com>
2024-05-09 12:38:24 -0400

    Added some helper methods for determining if the event has ended by datetime.

--- TeamBuilder/Models/TeamBuilder.cs
+++ TeamBuilder/Models/TeamBuilder.cs
@@ -9,7 +9,7 @@
         Flatten object graphs that contain nested objects. Flattened object graphs can be more convenient for clients.".
         Ref: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&tabs=visual-studio
 
-        Will map to Entity SQL eventually using DBContext and POCO, for now let's get it working!
+        Will map to a code-first approach using Entity Framework SQL,  using DBContext, and POCO's, eventually. For now let's get it working!
         A DbSet represents the collection of all entities in the context, or that can be queried from the database, of a given type. 
         DbSet objects are created from a DbContext.
 
@@ -22,9 +22,30 @@
         public long Id { get; set; }
         public string EventName { get; set; } = "";
         public string EventDescription { get; set; } = "";
-        public DateTime? EventDateTime { get; set; }
-        public bool IsEventOpen { get; set; }
-        public bool IsEventRecurring { get; set; }
+        public DateTime EventStart { get; set; }
+
+        public DateTime EventEnd { get; set; }
+        public bool EventRecurring { get; set; } = false;
+
+        public double GetEventDurationInMinutes()
+        {
+            if (EventEnd > EventStart)
+            {
+                TimeSpan span = EventEnd.Subtract(EventStart);
+                return span.TotalMinutes;
+            }
+            else
+            {
+                //Todo log an error because the UI or input should not allow such a scenario.
+                return 0;
+            }
+        }
+
+        public bool HasEventEnded()
+        {
+            DateTime dtEventEnd = EventStart.AddMinutes(GetEventDurationInMinutes());
+            return EventEnd >= dtEventEnd;
+        }
 
     }
 
@@ -38,7 +59,6 @@
 
         public TeamBuilder(TeamBuilderEventDto teamBuilderEventDto)
         {
-            //TeamBuilder.ObjectToDto(teamBuilderEventDto);
             _ = Util.Util.CopyProperties(teamBuilderEventDto, this, ["Secret"]);
         }
 
