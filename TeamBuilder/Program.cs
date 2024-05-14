ee6ec8af712da801838c423e80fc4ae437343735
TeamBuilderApp <teambuilderapp26@gmail.com>
2024-05-14 05:50:58 -0400

    Attempting to connect to Azure code first database using migrations. Added use of a appsettings.Development.json. No passwords checked into github.

--- TeamBuilder/Program.cs
+++ TeamBuilder/Program.cs
@@ -7,8 +7,17 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 //This section below is for connection string 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<TeamBuilderContext>(options => options.UseSqlServer(connectionString));
+var connection = String.Empty;
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+    connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+}
+else
+{
+    connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+}
+builder.Services.AddDbContext<TeamBuilderContext>(options => options.UseSqlServer(connection));
 //builder.Services.AddDbContext<TeamBuilderContext>(opt => opt.UseInMemoryDatabase("TeamBuilder"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
